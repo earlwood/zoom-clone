2df76c099d4767d70ba023e99e280bfb1373ac4d
Earl Wood <earl.wood@hotmail.com>
2022-08-05 21:27:37 -0500

    updating Angular version to 11

--- src/app/components/menu-bottom/menu-bottom.component.spec.ts
+++ src/app/components/menu-bottom/menu-bottom.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MenuBottomComponent } from './menu-bottom.component';
 
@@ -6,7 +6,7 @@ describe('MenuBottomComponent', () => {
   let component: MenuBottomComponent;
   let fixture: ComponentFixture<MenuBottomComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MenuBottomComponent ]
     })
